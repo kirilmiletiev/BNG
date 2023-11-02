3c83cdc97233d24e574a0a32e4e310bf5f6847f8
Niki <nikolaimiletievv@abv.bg>
2023-11-02 05:09:05 +0200

    .

--- src/contracts/Compare.sol
+++ src/contracts/Compare.sol
@@ -1,20 +1,18 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+// // SPDX-License-Identifier: MIT
+// pragma solidity ^0.8.20;
 
-// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.7.3/contracts/token/ERC20/ERC20.sol";
-// import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-// import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
-// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.7.3/contracts/token/ERC20/ERC20.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.7.3/contracts/token/ERC20/ERC20.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.7.3/contracts/token/ERC20/extensions/ERC20Capped.sol";
+pragma solidity >=0.7.0 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 
 contract Compare is ERC20Capped {
-    address public owner;
+    address payable public owner;
 
     constructor(
         uint256 cap
     ) ERC20("Compare", "CMR") ERC20Capped(cap * 10 ** decimals()) {
-        owner = msg.sender;
+        owner = payable(msg.sender);
         _mint(owner, 700000000 * (10 ** decimals()));
     }
 }
