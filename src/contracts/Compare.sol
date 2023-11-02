376a7f3a8aeecd40f9a8280dca5e11560091cd67
Niki <nikolaimiletievv@abv.bg>
2023-11-02 03:31:42 +0200

    delete  lock files

--- src/contracts/Compare.sol
+++ src/contracts/Compare.sol
@@ -1,13 +1,16 @@
+//pragma solidity >=0.7.0 <0.9.0;
+
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.7.0 <0.9.0;
+pragma solidity ^0.8.17;
 
-
-
-import {ERC20} from ;
-
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.7.3/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol"
 
 contract Compare is ERC20 {
-    constructor(uint, initialSupply) ERC20("Compare", "CMR"){
-        _mint(msg.sender, initialSupply);
+    address payable public owner;
+    constructor() ERC20("Compare", "CMR"){
+       owner = msg.sender;
+        _mint(owner, 70000000 (10 ** decimals()));
+
     }
 }
